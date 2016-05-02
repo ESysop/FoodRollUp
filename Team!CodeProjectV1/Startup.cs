675b845d9a1641b68d1a5e6586ed88a641a6f3f4
esysop <leehassel@gmail.com>
2016-05-02 11:14:30 -0500

    Restored I think

---
+++ Team!CodeProjectV1/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Team_CodeProjectV1.Startup))]
+namespace Team_CodeProjectV1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+	//test
+}
